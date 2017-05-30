9e69e6030e8a9445b394e55e537e5e6a5b09435d
Yeongeun Song <yeongeun.song@insertcoin.me>
2017-05-31 00:00:35 +0900

    #001_CalendarManager 헤더 , 소스 추가.

---
+++ ModuleTest/ios/CalendarManager.h
@@ -0,0 +1,17 @@
+//
+//  CalendarManager.h
+//  ModuleTest
+//
+//  Created by Yeongeun Song on 2017. 5. 30..
+//  Copyright © 2017년 Facebook. All rights reserved.
+//
+
+#ifndef CalendarManager_h
+#define CalendarManager_h
+
+#import <React/RCTBridgeModule.h>
+
+@interface CalendarManager : NSObject <RCTBridgeModule>
+@end
+
+#endif /* CalendarManager_h */
---
+++ ModuleTest/ios/CalendarManager.m
@@ -0,0 +1,17 @@
+//
+//  CalendarManager.m
+//  ModuleTest
+//
+//  Created by Yeongeun Song on 2017. 5. 30..
+//  Copyright © 2017년 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CalendarManager.h"
+
+@implementation CalendarManager
+
+// To export a module named CalendarManager
+RCT_EXPORT_MODULE();
+
+@end
